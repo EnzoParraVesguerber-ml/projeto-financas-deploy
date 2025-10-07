7cdd9e87fcf313f40cfdb03b2d0288b1031e3c7b
Bruno Lopes Torres <bruno.ltorres@hotmail.com>
2025-10-07 11:19:14 -0300

    feito o gráfico base para a página de resultados

--- pagina de resultados/script.js
+++ pagina de resultados/script.js
@@ -3,3 +3,32 @@ document.getElementById("logout-btn").addEventListener("click", function( event 
     window.location.href = "C:\\Users\\bruno\\Documents\\Facens\\2° Semestre\\Web Design\\Codigos html\\Projeto Finanças\\pagina inicial\\index.html";
 }, false);
 
+const labels= ['Casa e Vestuário', 'Veículo', 'Cuidados Pessoais', 'Supermercado', 'Alimentação', 'Educação', 'Saúde', 'Lazer e Eletrônicos', 'Pet', 'Serviços e Taxas', 'Transporte', 'Outros'];
+const data = {
+    labels: labels,
+    datasets: [{
+    label: 'Gastos Mensais por Categoria',
+    backgroundColor: 'rgba(54, 162, 235, 0.5)', // Cor das barras
+    borderColor: 'rgb(54, 162, 235)',         // Cor da borda das barras
+    borderWidth: 1,
+    data: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10], // Os valores de cada barra
+ }]
+};
+
+// Configurações do gráfico
+const config = {
+  type: 'bar', // Tipo do gráfico: 'bar' para barras
+  data: data,
+  options: {
+    responsive: true, // Torna o gráfico responsivo
+    scales: {
+      y: {
+        beginAtZero: true // Garante que o eixo Y comece no zero
+      }
+    }
+  }
+};
+
+const myChart = document.getElementById('resultsChart');
+
+new Chart(myChart, config);
