2bae070f95fad00f9b7e207cd75af084e1331859
Bruno Lopes Torres <bruno.ltorres@hotmail.com>
2025-10-07 21:51:32 -0300

    adicionados menus interativos para as outras páginas

--- pagina de signup/script.js
+++ pagina de signup/script.js
@@ -1,3 +1,14 @@
+const menuContainer = document.querySelector('.menu-container');
+const btnsLog = document.querySelector('.btns-log');
+
+menuContainer.addEventListener('mouseenter', function() {
+    btnsLog.classList.add('active');
+});
+
+menuContainer.addEventListener('mouseleave', function() {
+    btnsLog.classList.remove('active');
+});
+
 document.getElementById("login-btn").addEventListener("click", function( event ){
     event.preventDefault();
     window.location.href = "C:\\Users\\bruno\\Documents\\Facens\\2° Semestre\\Web Design\\Codigos html\\Projeto Finanças\\pagina de login\\index.html";
